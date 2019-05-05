9aea7d00518f813be5d3da8ebaaacd6fccbe1f4d
Felix <felix_krueger789@yahoo.de>
2019-05-05 21:20:14 +0200

    This is to add the min functionality to the test_unittest.py

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -108,3 +108,39 @@ def test_max_data_frame_array():
     
     assert output_max == expected_output_max
 
+
+def test_min_data_frame_list():
+
+    x = {
+        "a": [9, 2, 5, 8],
+        "b": [True, False, False, True],
+        "c": ["rasds", "sdsd", "cds", "sd"],
+        "d": [1.4, 1.5, 3.6, 1.1, 1.3],
+    }
+    df_cl = DataFrame(x)
+
+    expected_output_min = [np.min(x["a"]), np.min(x["d"])]
+
+    output_min = df_cl.min()
+
+    assert output_min == expected_output_min
+
+
+def test_min_data_frame_array():
+
+    y = {
+        "a": np.array([9, 2, 5, 8]),
+        "b": np.array([True, False, False, True]),
+        "c": np.array(["rasds", "sdsd", "cds", "sd"]),
+        "d": np.array([1.4, 1.5, 3.6, 1.1, 1.3]),
+    }
+    df_cl_y = DataFrame(y)
+
+    expected_output_min = [np.min(y["a"]), np.min(y["d"])]
+
+    output_min = df_cl_y.min()
+    
+    assert output_min == expected_output_min
+
+
+
